53575e4a66bf2644bcbc7061b365dba88bb838aa
Henry Desroches <yo@henry.codes>
2020-05-05 10:05:20 -0600

    🎥  - Add base scene as a demonstration of how imports work, etc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-// You can use import statements here! 
+// You can use import statements here!
+//
+// More information about how to do this here:
+// https://threejs.org/docs/#manual/en/introduction/Import-via-modules
+
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  IcosahedronGeometry,
+  MeshNormalMaterial,
+  Mesh,
+} from "three";
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const geometry = new IcosahedronGeometry();
+const material = new MeshNormalMaterial();
+const mesh = new Mesh(geometry, material);
+scene.add(mesh);
+
+camera.position.z = 5;
+
+const renderer = new WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
